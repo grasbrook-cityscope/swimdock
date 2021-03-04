72f6b0a47c54d378a3df34aaf9ca37a04da74011
andredaa <post.for.andre@gmail.com>
2021-03-04 23:03:18 +0100

    Compile swmm dependency from source in dockerfile Fix imports

--- Dockerfile
+++ Dockerfile
@@ -4,23 +4,44 @@ RUN apt-get update && apt-get upgrade -y
 
 RUN apt-get install -y unzip wget cmake
 
+RUN apt-get install python3 python3-pip -y
+RUN apt-get install git -y
+RUN apt-get install build-essential -y
+# RUN apt-get install cmake -y
+RUN pip3 install scikit-build
+
+RUN apt-get install build-essential libssl-dev -y
+RUN wget https://github.com/Kitware/CMake/archive/v3.20.0-rc3.tar.gz
+RUN tar -zxvf v3.20.0-rc3.tar.gz
+WORKDIR /CMake-3.20.0-rc3
+RUN ls
+RUN ./bootstrap
+RUN make
+RUN make install
 
 RUN mkdir /app
 WORKDIR /app
 
-# get SWMM
-RUN wget -O swmm.zip https://www.epa.gov/sites/production/files/2020-08/swmm51015_engine.zip
+# build swmm-python from source [installation buggy, PR pending]
+RUN git clone https://github.com/OpenWaterAnalytics/swmm-python.git
 
-# build SWMM
-RUN unzip swmm.zip
-WORKDIR /app/swmm51015_engine
-RUN mkdir build
-WORKDIR /app/swmm51015_engine/build
-RUN cmake ..
-RUN cmake --build . --config Release
+RUN apt-get install swig -y
+
+WORKDIR /app/swmm-python
+
+RUN git checkout dev-workflow
+RUN git submodule init
+RUN git submodule update
+
+WORKDIR /app/swmm-python/swmm-toolkit
+
+RUN python3 setup.py build
+RUN python3 setup.py install
+
 
 # run SWMM
 COPY ./app /app
 WORKDIR /app
+RUN python3 -m pip install -r requirements.txt
 ENTRYPOINT [ "python3", "main.py" ]
 CMD [] 
--- app/main.py
+++ app/main.py
@@ -3,22 +3,22 @@ import json
 import os
 import time
 
-from app.make_inp_file import make_inp_file
-from app.make_result_geojson import get_result_geojson
+from make_inp_file import make_inp_file
+from make_result_geojson import get_result_geojson
 
 known_hashes = {}
 user_id = None
 
 cwd = os.getcwd()
 data_dir = os.path.dirname(cwd) + "/data/"
-cityPyoUrl = 'http://localhost:5000/'
+cityPyoUrl = 'http://localhost:5000/'   # TODO update to nc.hcu-hamburg.de/cityPyo
 
 
 # login to cityPyo using the local user_cred_file
 # saves the user_id as global variable
 def cityPyo_login():
+    print("login in to cityPyo")
     with open(cwd + "/" + "cityPyoUser.json", "r") as user_cred_file:
-
         user_cred = json.load(user_cred_file)
 
     response = requests.post(cityPyoUrl + "login", json=user_cred)
@@ -53,8 +53,10 @@ def get_stormwater_scenarios():
 
 # creates an input file from user_input and run the simulation
 def perform_swmm_analysis(user_input):
+    print("making input file")
     make_inp_file(user_input)
 
+    print("computing")
     from swmm.toolkit import solver
     solver.swmm_run('../data/scenario.inp', '../data/scenario.rpt', '../data/scenario.out')
 
@@ -74,8 +76,7 @@ def send_response_to_cityPyo(scenario_hash):
         if not response.status_code == 200:
             print("could not post to cityPyo")
             print("Error code", response.status_code)
-            # todo raise error and return error
-
+            # todo : we need an error tracker!
         else:
             print("\n")
             print("result send to cityPyo.", "Result hash is: ", scenario_hash)
@@ -106,7 +107,6 @@ if __name__ == "__main__":
                 compute = True
 
             if compute:
-                print("computing")
                 perform_swmm_analysis(scenarios[scenario_id])
                 send_response_to_cityPyo(scenarios[scenario_id]["hash"])
                 known_hashes[scenario_id] = scenarios[scenario_id]["hash"]
