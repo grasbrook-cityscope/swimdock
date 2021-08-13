e9dcb18acc8f21eabc3a500c7c89cd4028162c80
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2021-08-13 11:58:43 +0200

    fix: test paths

--- app/test_users.py
+++ app/test_users.py
@@ -2,4 +2,4 @@ import users
 
 
 def test_readUserCredentialsFromFile():
-    assert users.readUserCredentialsFromFile("app/_test_fixtures/fixturePyoUser.json") == {'users': [{'password': 'blubb', 'username': 'testuser'}]}
+    assert users.readUserCredentialsFromFile("_test_fixtures/fixturePyoUser.json") == {'users': [{'password': 'blubb', 'username': 'testuser'}]}
